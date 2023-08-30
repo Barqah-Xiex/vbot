6161cd480dccc5b2611e3d20bf9b9e750fbcd338
Barqah <71264201+Barqah-Xiex@users.noreply.github.com>
2023-08-31 05:35:43 +0700

    Delete addons directory

--- addons/limitchat.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(Barqah){
-    setInterval(function(){
-        const {glimit_chat: a} = global;
-        Object.keys(a).forEach(v => {
-            if(v >= 0) return global.glimit_chat[v] = Number(`${global.glimit_chat[v]}`) - (Barqah.config.limit_chat/5);
-            global.glimit_chat[v] = undefined;
-        })
-    },10_000)
-}
--- addons/uploadkontak.js
+++
@@ -1,8 +0,0 @@
-module.exports = function ({db: {write},ev,decodeJid,func: {fs}}) {
-    ev.on('contacts.update', update => {
-        for (let contact of update) {
-            let id = decodeJid(contact.id)
-            write(`vbot/nama/${id}`,`${JSON.stringify({id,name: contact.notify},null,2)}`); 
-        }
-    })
-}
